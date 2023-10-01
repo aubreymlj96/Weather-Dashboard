c3f231a97b005506bee5038b0d75a036933255a8
aubrey johnson <aubreymlj96@gmail.com>
2023-10-01 17:33:22 -0400

    Draft of Weather Dashboard - still need to work on the search history aspect and add url to readme

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# Weather-Dashboard
+# AJ-Weather-Dashboard
+
+## Your Task
+
+Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+
+**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+
+You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Website URL
+
+
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The weather app includes a search option and a list of previously searched cities.](./image-ref/AJ%20Weather%20Dashboard%20View%201.png)
+
+![The weather app includes a five-day forecast and current weather conditions for Hong Kong.](./image-ref/AJ%20Weather%20Dashboard%20View%202%20-%20post%20search.png)
+
+## Installation
+
+The HTML contributes the following:
+
+-The structure and position of the elements on the webpage itself (headers, search box, sectioning for 'today's forecase' and '5-day forecast). The HTML also stores the links for jquery, bootstrap, css sheet, and the javascript file.
+
+The CSS consists of the style choices on the page:
+
+-The font size/colors/family, border styles, and color of the elements.
+
+The Script contributes the following:
+
+-The functionality of the search button, search history, as well as the code for the weather API calls, which produces the forecast of the searched city.
+
+## Usage
+
+./image-ref/06-server-side-apis-homework-demo.png
+
+## Credits
+
+Erik (TA) assisted with setting up the search history.
+
+Web Source : Reference for use grid above - https://getbootstrap.com/docs/4.1/layout/grid/ 
+
+Web Source : Reference for fahrenheit symbol - https://stackoverflow.com/questions/3312001/degrees-symbol-as-in-degrees-celsius-fahrenheit-in-a-textview 
+
+## License
+
+MIT License
+
+Copyright (c) 2023 aubreymlj96
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ assets/script.js
@@ -0,0 +1,236 @@
+$(document).ready(function(){
+    $("#form-submit").submit(function(event){
+        getApi(event);
+        getFiveDaysApi(event);
+    });
+});
+
+let cityPicked = "";
+let citySearched = JSON.parse(localStorage.getItem('cities')) || [];
+
+function getApi(){
+    const requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityPicked + '&appid=8975142747d99cc3145ba3f9f2b188db'//'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=8975142747d99cc3145ba3f9f2b188db';
+
+    fetch(requestUrl)
+        .then(function(response){
+            return response.json();
+        }).then(function(data){
+            console.log(data);
+            let latitude = data[0].lat;
+            let longitude = data[0].lon;
+            getCurrentWeather(latitude, longitude);
+        })
+
+}
+
+var apiKey = '8975142747d99cc3145ba3f9f2b188db';
+
+function getCurrentWeather(lat, lon){
+    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+    
+    
+    fetch(weatherUrl)
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+        let resultDetails = $('<div>');
+        let day = $('#fetch-elm').addClass('date-of');
+        let date = new Date(data.dt * 1000);
+        let dayOf = $('<h3>').addClass('actual-date').text(date);
+        let expCity = $('<p>').addClass('exp-city').text('Current Weather for').append(cityPicked);
+        let degree = $('<p>').addClass('temperature').text('Current Temperature =' + (data.main.temp) + '\u2109');
+        let humid = $('<p>').addClass('humidity').text('Current Humidity =' + data.main.humidity + '%');
+        let windSpeed = $('<p>').addClass('wind').text('Current Wind Speed = ' + Math.round(data.wind.speed) + 'mph');
+        let icon = $(`<img>`).attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`).addClass("rounded d-block");     
+        console.log(day, dayOf, expCity, degree, humid, windSpeed, icon);
+        $('#fetch').append(resultDetails.append(day, dayOf, expCity, degree, humid, windSpeed, icon));
+        getFiveDaysApi(lat, lon);
+    })
+
+    
+}
+
+var getFiveDays = [];
+
+function getFiveDaysApi(lat, lon){
+    const urlRequest = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+    
+    fetch(urlRequest)
+        .then(function(response){
+            return response.json();
+        }).then(function(datas){
+            for(i = 0; i < datas.list.length; i += 8){
+                getFiveDays.push(datas.list[i]);
+            }
+            console.log(getFiveDays);
+            for(i = 0; i < getFiveDays.length; i++) {
+                let days = getFiveDays[i];
+                let cardDiv = document.getElementById('#fetch-five');
+                let card = $('<div>').addClass('col-sm-2 grid card min-vh-4');
+                const daysOftheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                //reference for use grid above - https://getbootstrap.com/docs/4.1/layout/grid/ 
+                let date2 = new Date(getFiveDays[i].dt_txt).getDay();
+                console.log(date2);
+                let dateOf = $('<h3>').addClass('actual-date').text(daysOftheWeek[date2]);
+                let cardDay = $('<h2>').text(getFiveDays[i].dt_txt);
+                let degree =  $('<p>').text('Current Temperature = ' + (getFiveDays[i].main.temp));
+                //source for fahrenheit symbol - https://stackoverflow.com/questions/3312001/degrees-symbol-as-in-degrees-celsius-fahrenheit-in-a-textview 
+                let humid = $('<p>').text('Current Humidity =' + getFiveDays[i].main.humidity + '%');
+                let windSpeed = $('<p>').text('Current Wind Speed =' + Math.round(getFiveDays[i].wind.speed) + 'mph');
+                let icon = $(`<img>`).attr('src', `https://openweathermap.org/img/wn/${getFiveDays[i].weather[0].icon}@2x.png`).addClass("rounded d-block");
+                $('#fetch-five').append(card.append(days, dateOf, cardDay, degree, humid, windSpeed, icon));
+                $('#five-days').append(cardDiv);
+            }
+          
+        })
+}
+
+function searchHistory(){
+    $('#searched').empty();
+    for(i = 0; i < citySearched.length; i++){
+        let past = $("<p class='cities'>");
+        past.attr('data', citySearched[i]);
+        past.text(citySearched[i]);
+        $('#searched').append(past);
+    }
+
+}
+
+$('#submitBtn').on('click', function(event){
+    event.preventDefault();
+    cityPicked = $('#city').val().trim();
+    console.log(citySearched);
+    // if((citySearched) == (cityPicked)){
+    //     (citySearched).push(cityPicked);
+
+    // }
+    // $(cityPicked).addClass('col-sm-2 card w-100');
+
+    // searchHistory();
+    getApi();
+    // citySearched = JSON.parse(localStorage.getItem('cities'));
+    citySearched.push(cityPicked);
+    localStorage.setItem('cities', JSON.stringify(citySearched));
+    $('city').val('');
+    getHistory();
+    // $('issues').css('block');
+});
+
+function getHistory(){
+    $('#city-list').empty();
+    for(i = citySearched.length-1; i >= 0; i--){
+        let button = $('<button>').addClass("row btn btn-primary m-2 p-2 history-button");
+        button.attr("id", citySearched[i]);
+        button.text(citySearched[i]);
+        $('#city-list').append(button);
+    }
+    $('.history-button').on('click', function(event){
+        cityPicked = event.target.id;
+        getApi();
+    })
+}
+
+// const searchTerm = document.getElementById("searched");
+// const cityEl = document.getElementById("city");
+
+// submitBtn.addEventListener("click", function () {
+//     const searchTerm = cityEl.value;
+//     getCurrentWeather(searchTerm);
+//     sendSearchHistory.push(searchTerm);
+//     localStorage.setItem("searched", JSON.stringify(citySearched));
+//     sendSearchHistory();
+// })
+
+// const historyEl = document.getElementById('history');
+
+// function sendSearchHistory() {
+//     historyEl.innerHTML = "";
+//     for (let i = 0; i < citySearched.length; i++) {
+//         const historyItem = document.createElement("input");
+//         historyItem.setAttribute("type", "text color-white");
+//         historyItem.setAttribute("class", "form-control d-block bg-gray");
+//         historyItem.setAttribute("value", sendSearchHistory[i]);
+//         historyItem.addEventListener("click", function () {
+//             getCurrentWeather(historyItem.value);
+//         })
+//         historyEl.append(historyItem);
+//     }
+// };
+
+
+// searchHistory();
+
+$(document).on('click', '.cities', function(){
+    cityPicked = $(this).text();
+    $(cityPicked).on('click', getApi)
+    getApi();
+    searchHistory();
+    refreshPage();
+    // getFiveDaysApi();
+});
+
+function searchHistory(){
+    var recentSearch = [];
+    recentSearch.push((citySearched).val());
+}
+
+
+function refreshPage(event){
+    $('#clear-history').on('click', '.cities', function(){
+    event.preventDefault();
+    localStorage.removeItem('city');
+    document.location.reload();
+})};
+
+getHistory();
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
